d2fe14e5d60cb884baa1b238618e93d4a5cba7aa
batarse1 <00014419@uca.edu.sv>
2023-06-20 15:42:13 -0600

    ✨ Update delimiter validation

--- src/utils/patterns.ts
+++ src/utils/patterns.ts
@@ -1,5 +1,5 @@
 const secret_validation = /^[a-z0-9A-Z\u00C0-\u00FF]*$/;
-const delimiter_validation = /^(;|,){1}$/;
+const delimiter_validation = /^.$/;
 const type_validation = /^(XML|JSON|TXT)$/;
 
 export { secret_validation, delimiter_validation, type_validation };
